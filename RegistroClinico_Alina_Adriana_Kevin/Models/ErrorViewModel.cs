9c255e840ffa9ba0082b404072235b3ce37c00c0
KevinJSS <kevin.alvarado@ucrso.info>
2022-07-26 11:54:48 -0600

    Commit inicial

---
+++ RegistroClinico_Alina_Adriana_Kevin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RegistroClinico_Alina_Adriana_Kevin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
