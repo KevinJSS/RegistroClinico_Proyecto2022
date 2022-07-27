a06333b8a90955d28d86846848e54ed73d5cca58
KevinJSS <103754829+KevinJSS@users.noreply.github.com>
2022-07-27 14:35:28 -0600

    Controlador e Index del Paciente

---
+++ RegistroClinico_Alina_Adriana_Kevin/Areas/Medic/Controllers/PatientController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RegistroClinico_Alina_Adriana_Kevin.Areas.Medic.Controllers
+{
+    [Area("Medic")]
+    public class PatientController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
