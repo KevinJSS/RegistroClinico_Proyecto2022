3e9bc57b0ca4f393d78a11c8f5cfd8213d4c4352
Adriana Arroyo <adriana.arroyo@ucrso.info>
2022-07-26 23:20:24 -0600

    Treatment with api added

--- RegistroClinico_Alina_Adriana_Kevin/Areas/Medic/Controllers/TreatmentController.cs
+++ RegistroClinico_Alina_Adriana_Kevin/Areas/Medic/Controllers/TreatmentController.cs
@@ -7,6 +7,7 @@ namespace ProyectoLenguajes_Adriana_Alina_Kevin.Areas.Admin.Controllers
     [Area("Medic")]
     public class TreatmentController : Controller
     {
+        #region HTTP GET POST
         private readonly IUnitOfWork _unitOfWork;
 
         public TreatmentController(IUnitOfWork unitOfWork)
@@ -18,6 +19,61 @@ namespace ProyectoLenguajes_Adriana_Alina_Kevin.Areas.Admin.Controllers
             IEnumerable<Treatment> treatmentList = _unitOfWork.Treatment.GetAll();
             return View(treatmentList);
         }
+
+        /* UPSERT GET */
+        public IActionResult Upsert(int? id)
+        {
+            Treatment treatment = new();
+
+            if (id != null || id > 0)
+            {
+                treatment = _unitOfWork.Treatment.GetFirstOrDefault(m => m.Id == id);
+            }
+
+            return View(treatment);
+        }
+
+        /* UPSERT POST */
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Treatment obj, IFormFile? file)
+        {
+            if (ModelState.IsValid)
+            {
+                if (obj.Id == 0)
+                    _unitOfWork.Treatment.Add(obj);
+                else
+                    _unitOfWork.Treatment.Update(obj);
+
+                _unitOfWork.Save();
+                TempData["success"] = "tratamiento guardado correctamente";
+            }
+            return RedirectToAction("Index");
+        }
+        #endregion
+
+        #region API
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var treatmentList = _unitOfWork.Treatment.GetAll();
+            return Json(new { data = treatmentList });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var obj = _unitOfWork.Treatment.GetFirstOrDefault(x => x.Id == id);
+
+            if (obj == null)
+                return Json(new { success = false, message = "Error al eliminar" });
+
+            _unitOfWork.Treatment.Remove(obj);
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "Eliminado correctamente" });
+        }
+        #endregion 
 
     }
 }
